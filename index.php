c4ad581d64d2d08ac7c9799971029037e7d1ad0a
SINtyanneru <irjitgd@gmail.com>
2024-01-13 04:18:03 +0900

    画像をだした

--- index.php
+++ index.php
@@ -3,6 +3,11 @@ ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 
 //画像を作る
-$IMG = imagecreatetruecolor(1000,60);
+$IMG = imagecreatetruecolor(100,60);
 imagefilledrectangle($IMG, 0, 0, 599, 399, 0x00000);
+
+//画像として出力
+header("Content-Type: image/png;");
+imagejpeg($IMG, NULL, 100);
+imagedestroy($IMG);
 ?>
